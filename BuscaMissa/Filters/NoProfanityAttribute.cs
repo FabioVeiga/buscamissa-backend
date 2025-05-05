89d84359abe1669e92d65f837a6177f57c8b684f
Fabio Veiga <fabinholveiga@gmail.com>
2025-05-05 10:02:10 -0300

    ajuste noprofanity

--- BuscaMissa/Filters/NoProfanityAttribute.cs
+++ BuscaMissa/Filters/NoProfanityAttribute.cs
@@ -5,7 +5,7 @@ namespace BuscaMissa.Filters
     public class NoProfanityAttribute : ValidationAttribute
     {
         // Lista de palavrões (adicione os que desejar)
-        private readonly string[] _bannedWords = new[] { "boquete", "Caralho", "Do caralho", "Foda", "Foda-se", "Foder", "Nem fodendo", "Pau", "Pica", "Porra", "Porra nenhuma", "Pra caralho", "Puta merda", "Puta que pariu", "Punheta", "Que porra é essa?", "Teu cu", "Trepar", "Olho do cú", "Buceta", "Xoxota", "Sacanagem", "Cacete", "Caceta", "Siririca" };
+        private readonly string[] _bannedWords = new[] { "boquete", "Caralho", "Do caralho", "Foda", "Foda-se", "Foder", "Nem fodendo", "Pau ", "Pica", "Porra", "Porra nenhuma", "Pra caralho", "Puta merda", "Puta que pariu", "Punheta", "Que porra é essa?", "Teu cu", "Trepar", "Olho do cú", "Buceta", "Xoxota", "Sacanagem", "Cacete", "Caceta", "Siririca" };
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
