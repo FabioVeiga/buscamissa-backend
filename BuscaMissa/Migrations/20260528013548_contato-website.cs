bd36b1f06a10c0d8b58d7a43dd3f79400eec7f13
fabioveiga <fabinholveiga@gmail.com>
2026-05-29 17:07:09 -0300

    migration

---
+++ BuscaMissa/Migrations/20260528013548_contato-website.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BuscaMissa.Migrations
+{
+    /// <inheritdoc />
+    public partial class contatowebsite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Website",
+                table: "Contatos",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Website",
+                table: "Contatos");
+        }
+    }
+}
